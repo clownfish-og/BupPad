3003efd62a0d7999d5ca728ef39f3a7561304475
clownfish-og <eriknimmo@gmail.com>
2024-10-02 11:28:47 -0400

    begin adding code for buppad09, keymap still needs work

--- QMK/buppad/buppad09/keymaps/default/keymap.c
+++ QMK/buppad/buppad09/keymaps/default/keymap.c
@@ -24,5 +24,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT(RGB_RMOD, RGB_VAI, RGB_MOD,
                RGB_HUI, KC_TRNS, RGB_SAI,
                RGB_HUD, RGB_VAD, RGB_SAD),
+               
+  [2] = LAYOUT(KC_KP_7, KC_KP_8, KC_KP_9,
+               KC_KP_4, LT(1, KC_KP_5), KC_KP_6,
+               KC_KP_1, KC_KP_2, KC_KP_3),
+
+  [3] = LAYOUT(RGB_RMOD, RGB_VAI, RGB_MOD,
+               RGB_HUI, KC_TRNS, RGB_SAI,
+               RGB_HUD, RGB_VAD, RGB_SAD),
+
+  [4] = LAYOUT(KC_KP_7, KC_KP_8, KC_KP_9,
+               KC_KP_4, LT(1, KC_KP_5), KC_KP_6,
+               KC_KP_1, KC_KP_2, KC_KP_3),
+
+  [5] = LAYOUT(RGB_RMOD, RGB_VAI, RGB_MOD,
+               RGB_HUI, KC_TRNS, RGB_SAI,
+               RGB_HUD, RGB_VAD, RGB_SAD),                             
 
 };
